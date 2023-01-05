7163402ab065ea320831e13a174935b1c559034c
shourovr82 <shafinur512@gmail.com>
2023-01-05 23:27:15 +0600

    new details added

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -5,6 +5,9 @@ const ProductDetails = () => {
     <div>
       <h2>Product Details</h2>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea voluptatum perspiciatis sed odit dolor harum cum debitis corporis, fuga enim recusandae deleniti ducimus aut in aliquam fugiat voluptatibus quae totam.</p>
+      <div>
+        <p className='text-3xl'>Product Details</p>
+      </div>
     </div>
   );
 };
