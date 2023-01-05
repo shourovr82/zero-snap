7819ffb29c5095c55044f7e661f32e93c9401ae6
shourovr82 <shafinur512@gmail.com>
2023-01-05 23:28:52 +0600

    main layout bg changed

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -28,7 +28,7 @@ const MainLayout = () => {
 
               <div>
                 <div className="bg-white py-3  px-4 rounded-lg flex justify-around items-center ">
-                  <input type="text" placeholder="seach" className=" bg-gray-100 rounded-md  outline-none pl-2 ring-indigo-700 w-full mr-2 p-2" />
+                  <input type="text" placeholder="seach" className=" bg-gray-200 rounded-md  outline-none pl-2 ring-indigo-700 w-full mr-2 p-2" />
                   <span><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor ">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                   </svg></span>
