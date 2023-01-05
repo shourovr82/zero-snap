3adaa4fb7e38e566b520c4645f9e6e10b4ee9e3c
shourovr82 <shafinur512@gmail.com>
2023-01-05 23:26:37 +0600

    product details added

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -4,7 +4,7 @@ const ProductDetails = () => {
   return (
     <div>
       <h2>Product Details</h2>
-
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea voluptatum perspiciatis sed odit dolor harum cum debitis corporis, fuga enim recusandae deleniti ducimus aut in aliquam fugiat voluptatibus quae totam.</p>
     </div>
   );
 };
